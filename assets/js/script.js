902be0981d230917f009f251575e7667d8d70593
Gianmarco720 <orabonagianmarco379@gmail.com>
2022-10-28 15:33:56 +0200

    printed todo list on screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,32 @@ const { createApp } = Vue;
     createApp({
         data() {
             return{
-                
+                tasks: [
+                    {
+                        text: 'Fare gli esercizi',
+                        done: false,
+                    },
+
+                    {
+                        text: 'Comprare delle corde nuove',
+                        done: true,
+                    },
+
+                    {
+                        text: 'Cambiare le corde alla chitarra',
+                        done: false,
+                    },
+
+                    {
+                        text: 'Lavare la macchina',
+                        done: true
+                    },
+
+                    {
+                        text: 'Committare e pushare',
+                        done: false
+                    }
+                ]
             }
         }
     }).mount('#app');
